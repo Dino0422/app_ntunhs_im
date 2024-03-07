60e6fa4c0e792fc16659f33fe273b96b8e034f36
Dino0422 <dino920422tw@gmail.com>
2024-03-07 16:33:28 +0800

    AddFun

--- Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
+++ Guess_number/app/src/main/java/com/example/guess_number/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.guess_number
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -10,9 +12,11 @@ import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        handler = Handler(Looper.getMainLooper())
 
         val textView = findViewById<TextView>(R.id.textView)
         val result_textView = findViewById<TextView>(R.id.result)
@@ -28,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         guess_button.setOnClickListener{
             textView.text = editText.text
             valid_num = editText.text.toString().toInt()
-            var ans_str = "你猜對的囉!"
+            var ans_str = this.getString(R.string.yes_you_got_the_answer)
 
             if (valid_num > secret) {
                 if(guess_number[1] > valid_num) {
@@ -42,6 +46,16 @@ class MainActivity : AppCompatActivity() {
 
             if (valid_num != secret) {
                 ans_str = guess_number[0].toString() + "~" + guess_number[1].toString()
+                textView.text = ans_str
+            }else{
+                textView.text = ans_str
+                handler.postDelayed({
+                    Toast.makeText(this,"6秒後的操作執行！",Toast.LENGTH_SHORT).show()
+                    editText.setText("0")
+                    secret = Random.nextInt(range_num[1]) + range_num[0]
+                    guess_number  = intArrayOf(range_num[0], range_num[1])
+                    textView.text = this.getString(R.string.try_again)
+                },5000)
             }
             textView.text = ans_str
             //Toast.makeText(this, secret.toString(), Toast.LENGTH_SHORT).show()
@@ -50,8 +64,14 @@ class MainActivity : AppCompatActivity() {
 
         reset_button.setOnClickListener{
             editText.setText("0")
-            secret = Random.nextInt(range_num[1]) + range_num[0]
-            textView.text = "我們在猜一次!"
+            //secret = Random.nextInt(range_num[1]) + range_num[0]
+            secret = 1
+            guess_number  = intArrayOf(range_num[0], range_num[1])
+            textView.text = this.getString(R.string.try_again)
         }
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        handler.removeCallbacksAndMessages(null)
     }
 }
