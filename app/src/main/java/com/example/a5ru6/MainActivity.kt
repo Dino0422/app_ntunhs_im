0b617f0e4913fbda2cf8e3190d20b3ec8eefe8eb
Dino0422 <160824734+Dino0422@users.noreply.github.com>
2024-03-18 20:42:01 +0800

    HW3

--- app/src/main/java/com/example/a5ru6/MainActivity.kt
+++ app/src/main/java/com/example/a5ru6/MainActivity.kt
@@ -1,13 +1,15 @@
 package com.example.a5ru6
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.ViewParent
 import android.widget.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -41,15 +43,41 @@ class MainActivity : AppCompatActivity() {
             if (chkbox3.isChecked()){
                 msg = msg + "、"+ chkbox3.getText().toString()
             }
-            Toast.makeText(this@MainActivity,"你選的是" + msg,Toast .LENGTH_SHORT).show()
+            //Toast.makeText(this@MainActivity,"你選的是" + msg,Toast .LENGTH_SHORT).show()
+            val content = "ID:${findViewById<EditText>(R.id.editTextTextPersonName2).text.toString()}\n" +
+                    "pwd:${findViewById<EditText>(R.id.editTextTextPersonName3).text.toString()}\n" +
+                    "Name:${findViewById<EditText>(R.id.bth_send).text.toString()}\n" +
+                    "Birthday:${findViewById<EditText>(R.id.editTextDate2).text.toString()}\n" +
+                    "Gender:${findViewById<RadioButton>(radGrp_Gender.checkedRadioButtonId).text.toString()}"
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+            builder
+                .setMessage(content + "\n" + "興趣:${msg}")
+                .setTitle("輸入內容")
+                .setPositiveButton("確認") { dialog, _ -> }
+
+            val dialog: AlertDialog = builder.create()
+            dialog.show()
         }
 
-        applyDate.setOnClickListener{
+        applyDate.setOnClickListener {
             val calender = Calendar.getInstance()
-            val year = calender.get
+            val year = calender.get(Calendar.YEAR)
+            val month = calender.get(Calendar.MONTH)
+            val day = calender.get(Calendar.DAY_OF_MONTH)
+            DatePickerDialog(this, {_, year, month, day ->
+                run {
+                    var format = "${setDateFormat(year, month, day)}"
+                    applyDate.setText(format)
+                }
+            }, year, month, day).show()
         }
-    }
+
+
+        }
     private fun setDateFormat(year: Int, month: Int, day: Int): String{
         return "$year-${month + 1}-$day"
     }
 }
+
+
+
